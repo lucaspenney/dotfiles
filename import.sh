3786c1a62df078390e3de9c0b20d5db48d9ceb22
Lucas Penney <lucaspenney@gmail.com>
2014-12-04 14:46:14 -0800

    Add import script to add updated versions of files currently in dotfiles repo from home directory

---
+++ import.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin master;
+
+
+function doImport() {
+    find . -maxdepth 4 -type f -not -path "*git*" -not -name "import.sh" -not -name "README.md" -not -name "export.sh" | while read -r i
+    do
+        cp ~/$i ./$i
+    done
+}
+
+read -p "This will import the current computer's home directory files into the dotfiles repo.  Are you sure? (y/n) " -n 1;
+echo "";
+
+if [[ $REPLY =~ ^[Yy]$i ]]; then
+  doImport
+fi;
+
+unset doExport;
