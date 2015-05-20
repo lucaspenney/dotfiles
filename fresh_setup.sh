54844ce89a46d72ecd966f9e41061471516f6d51
lucas <lucaspenney@gmail.com>
2015-05-19 23:22:25 -0700

    Add fresh setup script for stuff to do on a fresh lubuntu install

---
+++ fresh_setup.sh
@@ -0,0 +1,8 @@
+#Set up system (after a fresh (likely lubuntu) install).
+
+sudo apt-get install human-theme
+sudo apt-get install human-icon-theme
+
+#Set appearance in lxappearance to theme (dark theme)
+
+
