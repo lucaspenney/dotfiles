cd4a2087929b9125e5d49c494534d4d00f2f13e9
Lucas Penney <lucaspenney@gmail.com>
2016-12-14 14:09:14 -0800

    Implement export using symlinks instead of file copying, update various settings

--- export.sh
+++ export.sh
@@ -2,6 +2,30 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
+directories=( .config .irssi .i3 .oh-my-zsh )
+for i in "${directories[@]}"
+do
+  files=(`cd ~/dotfiles/$i && find . -printf '/%P\n'`)
+  for k in "${files[@]}"
+  do
+    echo $i$k;
+    if [[ -d $i$k ]]; then
+      echo "Directory, skipping symlink"
+    else
+      echo "Symlinking $i$k"
+      ln -fs ~/dotfiles/$i$k ~/$i$k
+    fi
+  done
+done
+
+files=(.vimrc .xModmap .zshrc .bashrc)
+for f in "${files[@]}"
+do
+  echo $f
+  ln -fs ~/dotfiles/$f ~/$f
+done
+
+exit;
 git pull origin master;
 
 
