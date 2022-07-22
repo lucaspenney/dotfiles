0643f148789cd8fdff35381125d136a6e82e291b
Lucas Penney <lucaspenney@gmail.com>
2022-07-21 17:47:40 -0700

    polybar cleanup, add temps script

--- link.py
+++ link.py
@@ -9,22 +9,22 @@ dotfiles = home + "/Git/dotfiles"
 print("Linking files from " + dotfiles + " to " + home)
 
 files = [
-    '.config/i3/config',
-    '.config/i3/polybar',
-    '.config/i3/picom.conf',
-    '.config/i3/topbar.py',
-    '.config/i3/wallpaper.png',
-    '.config/i3/dropbox.sh',
-    '.vimrc',
-    '.zshrc',
-    '.bashrc',
-    '.Xmodmap',
+	'.config/i3/config',
+	'.config/i3/polybar',
+	'.config/i3/picom.conf',
+	'.config/i3/topbar.py',
+	'.config/i3/wallpaper.png',
+	'.config/i3/dropbox.sh',
+	'.vimrc',
+	'.zshrc',
+	'.bashrc',
+	'.Xmodmap',
 ]
 
 for f in files:
-    path = home + "/" + f
-    dpath = dotfiles + "/" + f
-    print("Linking " + dpath + " to " + path)
-    subprocess.run(['ln', '-sf', dpath, path])
+	path = home + "/" + f
+	dpath = dotfiles + "/" + f
+	print("Linking " + dpath + " to " + path)
+	subprocess.run(['ln', '-sf', dpath, path])
 
-pass
+print("All done.")
---
+++ scripts/temps.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+
+cmd = subprocess.run(['hostname'], capture_output=True, text=True)
+hostname = cmd.stdout.strip()
+
+output = ""
+
+if hostname == "laptop":
+	cmd = subprocess.run(['sensors'], capture_output=True, text=True)
+	sensors = cmd.stdout.strip()
+	groups = sensors.split("\n\n")
+
+	for s in groups:
+		lines = s.split("\n")
+		if lines[0] == "k10temp-pci-00c3":
+			temp = lines[2]
+			temp = temp[temp.index('+'):]
+			temp = temp[1:]
+			output = temp
+
+print(output)
